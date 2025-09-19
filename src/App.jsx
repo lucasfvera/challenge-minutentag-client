8c620224184e2eb17017b16fb00ceede440baac3
lucasfvera <lucasferreyravera@gmail.com>
2025-09-19 14:24:13 -0300

    feat: Add routing skeleton structure
    This commit adds the Wouter library to handle client side routing. It also adds the basic routes for our project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Route, Switch } from 'wouter';
 import './App.css';
 
 function App() {
-    return <>E-commerce Beer</>;
+    return (
+        <Switch>
+            {/* <h1>E-commerce Beer</h1> */}
+            <Route
+                path={'/products'}
+                component={() => <div>Products page</div>}
+            />
+            <Route
+                path={'/products/:productId-:productBrand'}
+                component={(params) => (
+                    <div>
+                        Products page:{' '}
+                        {params.params['productId-:productBrand']}
+                    </div>
+                )}
+            />
+            <Route>404 - Not Found</Route>
+        </Switch>
+    );
 }
 
 export default App;
